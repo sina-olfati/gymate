d271ba6fdec48b835601cabb005620cf455b3d24
sina <sinaolfati5@gmail.com>
2023-08-23 11:57:27 +0330

    initialized project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Nav/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
